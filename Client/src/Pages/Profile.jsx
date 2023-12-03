e51f9dff05f55eac77c15ceff6c973b83e66e2b3
AmirMuse <amirlearn08@gmail.com>
2023-12-03 11:13:45 -0800

    syles profile page

--- Client/src/Pages/Profile.jsx
+++ Client/src/Pages/Profile.jsx
@@ -2,48 +2,46 @@ import React from "react";
 import { useSelector } from "react-redux";
 export default function Profile() {
   const { currentUser } = useSelector((state) => state.user);
-  return (
-    <>
-      <div className="max-w-lg mx-auto p-3 my-7">
-        <h1 className="text-center text-4xl mb-5">Profile</h1>
 
-        <form className="flex  flex-col gap-2   justify-center items-center">
-          <img
-            src={currentUser.avatar}
-            className="w-[80px] h-[80px] object-cover object-center   rounded-full  cursor-pointer "
-          />
-          <input
-            type="text"
-            className="p-3  border  rounded-lg w-full"
-            placeholder="username"
-          />
-          <input
-            type="text"
-            className="p-3  border  rounded-lg w-full"
-            placeholder="Email"
-          />
-          <input
-            type="text"
-            className="p-3  border  rounded-lg w-full"
-            placeholder="Password"
-          />
-          <input
-            type="submit"
-            className="p-3  border  rounded-lg w-full bg-blue-900 text-white text-xl"
-          />
-          <button className="bg-green-900 w-full text-white p-3 text-xl rounded-lg">
-            Create Listings
-          </button>
-          <div className="w-full  flex justify-between p-3 text-red-900">
-            <div className="">
-              <button className="text-red-700">sign out</button>
-            </div>
-            <div className="">
-              <button className="text-red-700">sign out</button>
-            </div>
-          </div>
-        </form>
+  return (
+    <div className="max-w-lg p-3 my-7 mx-auto">
+      <h1 className="text-center text-xl ">Profile</h1>
+      <div className="flex justify-center  my-4">
+        <img src="" className="rounded-full  bg-blue-900  h-24 w-24 " alt="" />
       </div>
-    </>
+
+      <form action="" className="flex flex-col gap-2 ">
+        <input
+          type="text"
+          placeholder="Enter Your Name"
+          className="rounded-lg  h-[40px] pl-3"
+        />
+        <input
+          type="email"
+          placeholder="Enter Your email"
+          className="rounded-lg  h-[40px] pl-3"
+        />
+        <input
+          type="password"
+          placeholder="Enter Your password"
+          className="rounded-lg  h-[40px] pl-3"
+        />
+        <input
+          type="submit"
+          value="submit"
+          className="rounded-lg  pl-3 bg-blue-900 text-white p-3 text-xl"
+        />
+        <input
+          type="button"
+          value="Create Listings"
+          className="rounded-lg  pl-3 bg-green-900 text-white p-3 text-xl"
+        />
+        <div className="flex justify-between">
+          <p className="text-red-900">Sign Out</p>
+          <p className="text-red-900 ">Listengs</p>
+        </div>
+        {/* <input type="text" placeholder='Enter Your Name' /> */}
+      </form>
+    </div>
   );
 }
